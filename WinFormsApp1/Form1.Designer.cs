e5fda8eec95037f2acff685dd0d2c1b6c16b14ba
Muravev Ivan <muraviev.ivan04@gmail.com>
2024-11-04 00:49:58 +0300

    added labels for fields

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -35,6 +35,9 @@ partial class Form1
         _decodeRadioButton = new RadioButton();
         _methodsGroupBox = new GroupBox();
         _resultTextBox = new TextBox();
+        _inputLabel = new Label();
+        _keyLabel = new Label();
+        _resultLabel = new Label();
         _methodsGroupBox.SuspendLayout();
         SuspendLayout();
         // 
@@ -47,14 +50,14 @@ partial class Form1
         // 
         // _keyTextBox
         // 
-        _keyTextBox.Location = new Point(40, 280);
+        _keyTextBox.Location = new Point(40, 300);
         _keyTextBox.Name = "_keyTextBox";
         _keyTextBox.Size = new Size(400, 35);
         _keyTextBox.TabIndex = 1;
         // 
         // _processButton
         // 
-        _processButton.Location = new Point(40, 450);
+        _processButton.Location = new Point(40, 480);
         _processButton.Name = "_processButton";
         _processButton.Size = new Size(142, 40);
         _processButton.TabIndex = 2;
@@ -64,7 +67,7 @@ partial class Form1
         // _encodeRadioButton
         // 
         _encodeRadioButton.AutoSize = true;
-        _encodeRadioButton.Location = new Point(6, 47);
+        _encodeRadioButton.Location = new Point(6, 34);
         _encodeRadioButton.Name = "_encodeRadioButton";
         _encodeRadioButton.Size = new Size(116, 36);
         _encodeRadioButton.TabIndex = 3;
@@ -75,7 +78,7 @@ partial class Form1
         // _decodeRadioButton
         // 
         _decodeRadioButton.AutoSize = true;
-        _decodeRadioButton.Location = new Point(6, 89);
+        _decodeRadioButton.Location = new Point(6, 76);
         _decodeRadioButton.Name = "_decodeRadioButton";
         _decodeRadioButton.Size = new Size(116, 36);
         _decodeRadioButton.TabIndex = 4;
@@ -90,22 +93,52 @@ partial class Form1
         _methodsGroupBox.Controls.Add(_decodeRadioButton);
         _methodsGroupBox.Location = new Point(40, 20);
         _methodsGroupBox.Name = "_methodsGroupBox";
-        _methodsGroupBox.Size = new Size(200, 135);
+        _methodsGroupBox.Size = new Size(200, 122);
         _methodsGroupBox.TabIndex = 5;
         _methodsGroupBox.TabStop = false;
         _methodsGroupBox.Text = "Method";
         // 
         // _resultTextBox
         // 
-        _resultTextBox.Location = new Point(40, 360);
+        _resultTextBox.Location = new Point(40, 400);
         _resultTextBox.Name = "_resultTextBox";
         _resultTextBox.ReadOnly = true;
         _resultTextBox.Size = new Size(400, 35);
         _resultTextBox.TabIndex = 6;
         // 
+        // label1
+        // 
+        _inputLabel.AutoSize = true;
+        _inputLabel.Location = new Point(40, 160);
+        _inputLabel.Name = "label1";
+        _inputLabel.Size = new Size(84, 32);
+        _inputLabel.TabIndex = 7;
+        _inputLabel.Text = "Input";
+        // 
+        // label2
+        // 
+        _keyLabel.AutoSize = true;
+        _keyLabel.Location = new Point(40, 260);
+        _keyLabel.Name = "label2";
+        _keyLabel.Size = new Size(56, 32);
+        _keyLabel.TabIndex = 8;
+        _keyLabel.Text = "Key";
+        // 
+        // label3
+        // 
+        _resultLabel.AutoSize = true;
+        _resultLabel.Location = new Point(40, 360);
+        _resultLabel.Name = "label3";
+        _resultLabel.Size = new Size(98, 32);
+        _resultLabel.TabIndex = 9;
+        _resultLabel.Text = "Result";
+        // 
         // Form1
         // 
         ClientSize = new Size(784, 561);
+        Controls.Add(_resultLabel);
+        Controls.Add(_keyLabel);
+        Controls.Add(_inputLabel);
         Controls.Add(_resultTextBox);
         Controls.Add(_methodsGroupBox);
         Controls.Add(_processButton);
@@ -131,4 +164,7 @@ partial class Form1
     private RadioButton _decodeRadioButton;
     private GroupBox _methodsGroupBox;
     private TextBox _resultTextBox;
+    private Label _inputLabel;
+    private Label _keyLabel;
+    private Label _resultLabel;
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -17,7 +17,7 @@ public partial class Form1 : Form
     private void BindControls()
     {
         _inputTextBox.DataBindings.Add(nameof(_inputTextBox.Text), _viewModel, nameof(_viewModel.Input), true, DataSourceUpdateMode.OnPropertyChanged);
-        
+
         _keyTextBox.DataBindings.Add(nameof(_keyTextBox.Text), _viewModel, nameof(_viewModel.Key), true, DataSourceUpdateMode.OnPropertyChanged);
 
         _resultTextBox.DataBindings.Add(nameof(_resultTextBox.Text), _viewModel, nameof(_viewModel.Result), true, DataSourceUpdateMode.OnPropertyChanged);
@@ -25,7 +25,7 @@ public partial class Form1 : Form
         _processButton.DataBindings.Add(nameof(_processButton.Command), _viewModel, nameof(_viewModel.ProcessButtonCommand), true);
 
         _encodeRadioButton.DataBindings.Add(nameof(_encodeRadioButton.Command), _viewModel, nameof(_viewModel.EnterEncodeModeCommand), true);
-        
+
         _decodeRadioButton.DataBindings.Add(nameof(_decodeRadioButton.Command), _viewModel, nameof(_viewModel.EnterDecodeModeCommand), true);
     }
 }
